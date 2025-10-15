2abd5d1b8be42e8a3035dd435e54c202707f8def
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-10-15 16:55:10 -0500

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "ReactTwemojiPicker",
+      fileName: (format) => `react-twemoji-picker.${format}.js`,
+      formats: ["es", "cjs", "umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
