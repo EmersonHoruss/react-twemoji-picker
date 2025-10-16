77f56353e882f3a23770d776550eec6df1909148
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-10-16 13:12:48 -0500

    popup skins in progress

---
+++ src/components/PopupSkins.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./PopupSkins.css";
+import {
+  popperGenerator,
+  defaultModifiers,
+  type Instance,
+} from "@popperjs/core/lib/popper-lite";
+import offset from "@popperjs/core/lib/modifiers/offset";
+import preventOverflow from "@popperjs/core/lib/modifiers/preventOverflow";
+import arrow from "@popperjs/core/lib/modifiers/arrow";
+
+import useOnclickOutside from "react-cool-onclickoutside";
+
+import type EmojiSkin from "../interfaces/EmojiSkin";
+
+interface PopupSkinsProps {
+  disabled: boolean;
+  offset: [number, number];
+  emojiList: EmojiSkin[];
+  clickEmoji: (emoji: EmojiSkin) => void;
+
+  onPopperOpenChanged?: (val: boolean) => void;
+  onCloseOnClickawayChanged?: (val: boolean) => void;
+}
+
+const PopupSkins: React.FC<PopupSkinsProps> = ({
+  disabled = false,
+  offset = [0, 30],
+  emojiList = [],
+  clickEmoji,
+
+  onPopperOpenChanged,
+  onCloseOnClickawayChanged,
+}) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const buttonRef = useRef<HTMLElement | null>(null);
+  const [popperOpen, setPopperOpen] = useState(false);
+  const [debouncedPopperOpen, setDebouncedPopperOpen] = useState(false);
+  const [popperInstance, setPopperInstance] = useState<Instance | null>(null);
+  const [closeOnClickaway, setCloseOnClickaway] = useState(true);
+
+  useEffect(() => {
+    onPopperOpenChanged?.(popperOpen);
+
+    const timer = setTimeout(() => {
+      setDebouncedPopperOpen(popperOpen);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [popperOpen, onPopperOpenChanged]);
+
+  useEffect(() => {
+    onCloseOnClickawayChanged?.(closeOnClickaway);
+  }, [closeOnClickaway, onCloseOnClickawayChanged]);
+
+  
+
+  return (
+    <div>
+      <div ref={containerRef} id="popper-skins-container">
+        <div id="skins-arrow" data-popper-arrow />
+        <div id="popper-inner">
+          <div className="emoji-popover-inner">
+            {emojiList.length > 0 && (
+              <p className="emoji-list">
+                {emojiList.map((emoji) => (
+                  <span
+                    key={emoji.unicode}
+                    dangerouslySetInnerHTML={{ __html: emoji.img }}
+                    onClick={() => clickEmoji(emoji)}
+                  />
+                ))}
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopupSkins;
